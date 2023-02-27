95ec12dda6a2172d238454d60e8a3c9c17fd6183
Nathan COLIN <ncolin.development@gmail.com>
2023-02-27 22:10:11 +0100

    Init project from initializr

---
+++ src/main/java/org/worldskills/regionalmod1/Module1RegionalStageWorldskillsFranceApplication.java
@@ -0,0 +1,13 @@
+package org.worldskills.regionalmod1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1RegionalStageWorldskillsFranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module1RegionalStageWorldskillsFranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/worldskills/regionalmod1/Module1RegionalStageWorldskillsFranceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.worldskills.regionalmod1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1RegionalStageWorldskillsFranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
