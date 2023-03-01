6f99462b7b0585e869c714644e60a221217fc3b9
Nathan COLIN <ncolin.development@gmail.com>
2023-03-01 21:50:31 +0100

    Adds data

--- src/main/java/org/worldskills/regionalmod1/model/Grade.java
+++ src/main/java/org/worldskills/regionalmod1/model/Grade.java
@@ -1,11 +1,6 @@
 package org.worldskills.regionalmod1.model;
 
 public enum Grade {
-    SIXIEME,
-    CINQUIEME,
-    QUATRIEME,
-    TROISIEME,
-    SECONDE,
-    PREMIERE,
-    TERMINALE;
+    COLLEGE,
+    LYCEE;
 }
